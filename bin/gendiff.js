c88f1ef03877117617cc08e12e8cefc26e4aa9b7
arty <artgoncarov3@gmail.com>
2024-05-14 13:19:13 +0300

    task2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,8 +5,9 @@ const program = new Command();
 
 program
  .name('gendiff')
+ .arguments('<filepath1> <filepath2>')
  .version('0.0.1', '-V, --version', 'output the version number')
  .description('Compares two configuration files and shows a difference.')
- //.option('-h, --help', 'output usage information')
+ .option('-f, --format [type]', 'output format') 
 
  program.parse();
