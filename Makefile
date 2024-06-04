367db459710fbdae9f4f6e0934903b989f85d6a4
arty <artgoncarov3@gmail.com>
2024-06-04 15:31:34 +0300

    problem task5

--- Makefile
+++ Makefile
@@ -4,3 +4,6 @@ lint:
 	npx eslint .
 lint-fix:
 	npx eslint . --fix
+
+test:
+	NODE_OPTION=--experimental-vm-modules npx jest
---
+++ __tests__/genDiff.test.js
@@ -0,0 +1,22 @@
+import fs from 'fs';
+import path from "path";
+import { fileURLToPath } from "url";
+
+import pathFile from "../src/index.js";
+import test from "node:test";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);// текущ. местоположение
+
+const getFixturesPath = (filePath) => path.join(__dirname, '..', '__fixtures__', filePath);
+
+const fileData = (filePath) => fs.readFileSync(getFixturesPath(filePath), 'utf-8');
+
+const referenceFlatFiles = fileData('flat-files-test.txt');
+
+test('Flat JSON', () => {
+    const file1 =  getFixturesPath('file1.json');
+    const file2 = getFixturesPath('file2.json');
+
+    expect(pathFile(file1, file2)).toEqual(referenceFlatFiles);
+})
